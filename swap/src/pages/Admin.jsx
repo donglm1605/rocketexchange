60608aa7b40ae3848044e9d9d93de3f8b07d2a78
donglm1605 <116880839+donglm1605@users.noreply.github.com>
2023-06-08 01:34:37 +0700

    Update Admin.jsx

--- swap/src/pages/Admin.jsx
+++ swap/src/pages/Admin.jsx
@@ -1,56 +1,40 @@
-import React, { useEffect } from "react";
+import React, {useRef, useEffect } from "react";
 import CommonSection from "../components/ui/Common-section/CommonSection";
 import { Container, Row, Col } from "reactstrap";
 import "../styles/swap.css";
 
 const Admin = () => {
-    const addressRef = useRef("");
-    const priceRef = useRef("");
-    
+
+    const adressRef = useRef("");
+    const amountRef = useRef("");
   
     const handleSubmit = (e) => {
       e.preventDefault();
     };
+  
     useEffect(() => {
-        // 👇️ scroll to top on page load
-        window.scrollTo({ top: 0, left: 0, behavior: "smooth" })
-    }, [])
-
+      // 👇️ scroll to top on page load
+      window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
+    }, []);
+  
     return (
-        <>
-        <CommonSection tittle={"Admin add collection"} />
+      <>
+        <CommonSection tittle={"Admin"} />
         <section>
-            <Container>
+          <Container>
             <Row>
-                <Col lg='6' md='6' className="m-auto text-center">
-                <h2>Please type address collection</h2>
+              <Col lg='6' md='6' className="m-auto text-center">
+                <h2>Please type address</h2>
                 <div className="contact mt-4">
-                    <form onSubmit={handleSubmit}>
+                  <form onSubmit={handleSubmit}>
                     <div className="form_input">
-                        <input 
-                            type="text" 
-                            placeholder='Address Collection:' 
-                            ref={addressRef}
-                        />
+                      <input 
+                        type="text" 
+                        placeholder='Type Address:' 
+                        ref={adressRef}
+                      />
                     </div>
-                    <button 
-                        className="send__btn" 
-                        style={{
-                            border:'none', 
-                            padding:'7px 25px', 
-                            borderRadius:'5px',
-                            marginTop:'20px',
-                        }}
-                    >
-                        Add Adress
-                    </button>
-                    </form>
-                </div>
-                </Col>
-                <Col lg='6' md='6' className="m-auto text-center">
-                <h2>Token</h2>
-                <div className="contact mt-4">
-                    <form onSubmit={handleSubmit}>
+  
                     <div className="row currencyInput">
                                 <div className="col-md-6 numberContainer">
                                     <input
@@ -67,61 +51,34 @@ const Admin = () => {
                                     </div>
                                 </div>
                             </div>
-                    <button 
-                        className="send__btn" 
-                        style={{
-                            border:'none', 
-                            padding:'7px 25px', 
-                            borderRadius:'5px',
-                            marginTop:'20px',
-                        }}
-                    >
-                        Accept
-                    </button>
-                    </form>
-                </div>
-                </Col>
-                <Col lg='6' md='6' className="m-auto text-center">
-                <h2>Please add price</h2>
-                <div className="contact mt-4">
-                    <form onSubmit={handleSubmit}>
+  
                     <div className="form_input">
-                        <input 
-                            type="text" 
-                            placeholder='Price collection:' 
-                            ref={priceRef}
-                        />
+                      <input 
+                        type="text" 
+                        placeholder='Amount' 
+                        ref={amountRef}
+                      />
                     </div>
+  
                     <button 
-                        className="send__btn" 
-                        style={{
-                            border:'none', 
-                            padding:'7px 25px', 
-                            borderRadius:'5px',
-                            marginTop:'20px',
-                        }}
+                      className="send__btn" 
+                      style={{
+                        border:'none', 
+                        padding:'7px 25px', 
+                        borderRadius:'5px',
+                        marginTop:'20px',
+                      }}
                     >
-                        Accept
+                      Confirm
                     </button>
-                    </form>
+                  </form>
                 </div>
-                <button 
-                        className="send__btn" 
-                        style={{
-                            border:'none', 
-                            padding:'7px 25px', 
-                            borderRadius:'5px',
-                            marginTop:'20px',
-                        }}
-                    >
-                        Add Collection
-                    </button>
-                </Col>
+              </Col>
             </Row>
-            </Container>
+          </Container>
         </section>
-        </>
+      </>
     );
-    };
-
-export default Admin;
+  };
+  
+  export default Admin;
